ca83203a47c85c3d663c9029fcf3693cf05db32f
paalvibe <paalvibe>
2023-04-27 18:11:04 +0000

    Cleanup and simplify

--- code/02-Data-Engineering/pyspark/04-CreateMaterializedViews/1-CreateMaterializedViews.py
+++ code/02-Data-Engineering/pyspark/04-CreateMaterializedViews/1-CreateMaterializedViews.py
@@ -4,6 +4,15 @@ from pyspark.sql.functions import *
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC # Materialized views
+# MAGIC
+# MAGIC A materialized view in databricks:
+# MAGIC
+# MAGIC An up-to-date, aggregated view of information for use in BI and analytics without having to reprocess the full underlying tables, instead updating only where changes have come through.
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ### 1.  Execute notebook with common/reusable functions 
 
 # COMMAND ----------
@@ -106,7 +115,7 @@ dbutils.fs.rm(destDataDirRoot,recurse=True)
 
 # MAGIC %sql
 # MAGIC use taxi_db;
-# MAGIC 
+# MAGIC
 # MAGIC DROP TABLE IF EXISTS taxi_trips_mat_view;
 
 # COMMAND ----------
@@ -165,60 +174,6 @@ matViewDF = sql("""
     trip_year,
     trip_month
   FROM yellow_taxi_trips_unionable 
-UNION ALL
-  SELECT DISTINCT 
-    taxi_type,
-    vendor_id,
-    pickup_datetime,
-    dropoff_datetime,
-    store_and_fwd_flag,
-    rate_code_id,
-    pickup_location_id,
-    dropoff_location_id,
-    pickup_longitude,
-    pickup_latitude,
-    dropoff_longitude,
-    dropoff_latitude,
-    passenger_count,
-    trip_distance,
-    fare_amount,
-    extra,
-    mta_tax,
-    tip_amount,
-    tolls_amount,
-    ehail_fee,
-    improvement_surcharge,
-    total_amount,
-    payment_type,
-    trip_type,
-    vendor_abbreviation,
-    vendor_description,
-    trip_type_description,
-    month_name_short,
-    month_name_full,
-    payment_type_description,
-    rate_code_description,
-    pickup_borough,
-    pickup_zone,
-    pickup_service_zone,
-    dropoff_borough,
-    dropoff_zone,
-    dropoff_service_zone,
-    pickup_year,
-    pickup_month,
-    pickup_day,
-    pickup_hour,
-    pickup_minute,
-    pickup_second,
-    dropoff_year,
-    dropoff_month,
-    dropoff_day,
-    dropoff_hour,
-    dropoff_minute,
-    dropoff_second,
-    trip_year,
-    trip_month
-  FROM taxi_db.green_taxi_trips_curated 
 """)
 
 #Save as Delta
@@ -237,7 +192,7 @@ matViewDF.printSchema()
 
 # MAGIC %sql
 # MAGIC use taxi_db;
-# MAGIC 
+# MAGIC
 # MAGIC DROP TABLE IF EXISTS taxi_trips_mat_view;
 # MAGIC CREATE TABLE taxi_trips_mat_view
 # MAGIC USING DELTA
